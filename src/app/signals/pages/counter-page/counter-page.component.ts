76dfae46a87d33022e63afe54bcc402a00ee7920
florius88 <florsantosbueno@gmail.com>
2023-05-21 18:50:22 +0200

    Counter Signal

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
   styleUrls: ['./counter-page.component.css']
 })
 export class CounterPageComponent {
+
+  public counter = signal(10)
+  // Esta es una señal de sólo lectura
+  public squareCounter = computed(() => this.counter() * this.counter())
+
+  /**
+   * Hace un update de la señal
+   * @param value
+   */
+  increaseBy(value: number) {
+    /* Esta es una forma de hacerlo, pero más "fea"
+    this.counter.set(this.counter() + value) */
+    this.counter.update(current => current + value)
+  }
 
 }
