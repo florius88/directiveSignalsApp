7224ad4066ec6960cab38095839fb84d99550cdc
florius88 <florsantosbueno@gmail.com>
2023-05-21 18:08:33 +0200

    Side Menu

--- src/app/signals/signals.module.ts
+++ src/app/signals/signals.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SignalsRoutingModule } from './signals-routing.module';
 import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.component';
+
 import { CounterPageComponent } from './pages/counter-page/counter-page.component';
 import { UserInfoPageComponent } from './pages/user-info-page/user-info-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 
 @NgModule({
@@ -13,7 +15,8 @@ import { PropertiesPageComponent } from './pages/properties-page/properties-page
     SignalsLayoutComponent,
     CounterPageComponent,
     UserInfoPageComponent,
-    PropertiesPageComponent
+    PropertiesPageComponent,
+    SideMenuComponent
   ],
   imports: [
     CommonModule,
